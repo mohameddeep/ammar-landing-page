64018ad485815c48e71d05910f9cabafa91ff428
MostafaAbdelmajied <99348503+MostafaAbdelmajied@users.noreply.github.com>
2025-09-25 09:48:42 +0300

    namespace correct

--- app/Http/Controllers/Dashboard/User/UserController.php
+++ app/Http/Controllers/Dashboard/User/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Dashboard\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Dashboard\User\UserRequest;
-use App\Http\Services\Dashboard\user\UserService;
+use App\Http\Services\Dashboard\User\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -15,7 +15,7 @@ class UserController extends Controller
     {
         return $this->user->index($request);
     }
- 
+
 
     public function show($id)
     {
